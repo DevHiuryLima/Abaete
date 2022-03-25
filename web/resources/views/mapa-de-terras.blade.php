29b560404e450d4aae8eceebeb01decff72e64fe
Hiury Lima <limahiury6@gmail.com>
2022-03-25 10:28:36 -0300

    Adicionado uma nova biblioteca
    Além das tela home, mapa de terras e a tela de criar registro que já estava feita... também foi adicionado a biblioteca JavaScript de código aberto, Leaflet e mostrado o mapa em duas paginas.

--- web/resources/views/mapa-de-terras.blade.php
+++ web/resources/views/mapa-de-terras.blade.php
@@ -19,74 +19,8 @@
             </footer>
         </aside>
         
-        <div class="leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom" tabindex="0" style="width: 100%; height: 100%; position: relative;">
-            <div class="leaflet-pane leaflet-map-pane" style="transform: translate3d(0px, 0px, 0px);">
-                <div class="leaflet-pane leaflet-tile-pane">
-                    <div class="leaflet-layer " style="z-index: 1; opacity: 1;">
-                        <div class="leaflet-tile-container leaflet-zoom-animated" style="z-index: 18; transform: translate3d(0px, 0px, 0px) scale(1);">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/46/69.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(393px, 51px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/46/70.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(393px, 307px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/45/69.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(137px, 51px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/47/69.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(649px, 51px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/45/70.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(137px, 307px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/47/70.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(649px, 307px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/46/68.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(393px, -205px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/46/71.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(393px, 563px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/45/68.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(137px, -205px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/47/68.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(649px, -205px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/45/71.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(137px, 563px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/47/71.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(649px, 563px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/44/69.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(-119px, 51px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/48/69.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(905px, 51px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/44/70.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(-119px, 307px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/48/70.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(905px, 307px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/44/68.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(-119px, -205px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/48/68.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(905px, -205px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/44/71.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(-119px, 563px, 0px); opacity: 1;">
-                            <img alt="" role="presentation" src="https://a.tile.openstreetmap.org/7/48/71.png" class="leaflet-tile leaflet-tile-loaded" style="width: 256px; height: 256px; transform: translate3d(905px, 563px, 0px); opacity: 1;">
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="leaflet-pane leaflet-shadow-pane"></div>
-                
-                <div class="leaflet-pane leaflet-overlay-pane"></div>
-                
-                <div class="leaflet-pane leaflet-marker-pane">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(610px, 43px, 0px); z-index: 43;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(5001px, -1229px, 0px); z-index: -1229;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(5001px, -1229px, 0px); z-index: -1229;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(5001px, -1229px, 0px); z-index: -1229;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(5001px, -1229px, 0px); z-index: -1229;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(5001px, -1229px, 0px); z-index: -1229;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(446px, 163px, 0px); z-index: 163;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(448px, 157px, 0px); z-index: 157;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(351px, 144px, 0px); z-index: 144;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(352px, 141px, 0px); z-index: 141;">
-                    <img src="/static/media/map-marker.61520125.svg" class="leaflet-marker-icon leaflet-zoom-animated leaflet-interactive" alt="" tabindex="0" style="margin-left: -29px; margin-top: -68px; width: 58px; height: 68px; transform: translate3d(495px, 310px, 0px); z-index: 310;">
-                </div>
-                
-                <div class="leaflet-pane leaflet-tooltip-pane"></div>
-                <div class="leaflet-pane leaflet-popup-pane"></div>
-                <div class="leaflet-proxy leaflet-zoom-animated" style="transform: translate3d(11883.5px, 17925.6px, 0px) scale(64);"></div>
-            </div>
-            <div class="leaflet-control-container">
-                <div class="leaflet-top leaflet-left">
-                    <div class="leaflet-control-zoom leaflet-bar leaflet-control">
-                        <a class="leaflet-control-zoom-in" href="#" title="Zoom in" role="button" aria-label="Zoom in">+</a>
-                        <a class="leaflet-control-zoom-out" href="#" title="Zoom out" role="button" aria-label="Zoom out">−</a>
-                    </div>
-                </div>
+        <div id="map-container" class="leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom" tabindex="0" style="width: 100%; height: 100%; position: relative;">
 
-                <div class="leaflet-top leaflet-right"></div>
-                <div class="leaflet-bottom leaflet-left"></div>
-
-                <div class="leaflet-bottom leaflet-right">
-                    <div class="leaflet-control-attribution leaflet-control">
-                        <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>
-                    </div>
-                </div>
-            </div>
         </div>
         
         <a class="criar-aldeia" href="{{ route('terra-criar') }}">
@@ -97,4 +31,21 @@
         </a>
     </div>
 </div>
+
+<script>
+
+    // A variavel 'map' recebe a classe leaflet (l) ponto map
+    // passando a div do mapa para tudo que for criado ser dentro dela
+    // coloando a posição e também o nivel de zoom
+    var map = L.map(document.getElementById('map-container'), {
+        center: [-16.6954999,-49.444356], 
+        zoom: 7
+    });
+
+
+    // Crio a variavel que vai ser a base do meu mapa e estou criando uma layer a partir do open street map
+    var baseMap = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    });
+    baseMap.addTo(map);
+</script>
 @endsection
--- web/resources/views/master/master.blade.php
+++ web/resources/views/master/master.blade.php
@@ -15,6 +15,10 @@
   <link rel="icon" type="image/png" sizes="200x200" href="<?= asset('') ?>">
 
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+  <!-- Usando uma versão hospedada do Leaflet  -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 </head>
 <body>
   
