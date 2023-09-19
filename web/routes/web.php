1eeb7da057a0097506dae8540be78ce6757404ee
Hiury Lima <limahiury6@gmail.com>
2023-09-18 23:49:07 -0300

    Atualizando as sintaxe das rotas web.
    OBS: Atualizando de acordo com a atualização para o Laravel 9.

--- web/routes/web.php
+++ web/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\web\IndexController;
+use App\Http\Controllers\web\TerraController;
+use App\Http\Controllers\web\QuizController;
+use App\Http\Controllers\web\AdministradorController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,41 +20,41 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('/')->group(function () {
-    Route::get('/', 'web\IndexController@index')->name('home');
-    Route::get('/login', 'web\IndexController@redirecionaLogin')->name('login');
-    Route::post('/login', 'web\IndexController@login')->name('fazer-login');
-    Route::get('/logout', 'web\IndexController@logout')->name('logout');
+    Route::get('/', [IndexController::class, 'index'])->name('home');
+    Route::get('/login', [IndexController::class, 'redirecionaLogin'])->name('login');
+    Route::post('/login', [IndexController::class, 'login'])->name('fazer-login');
+    Route::get('/logout', [IndexController::class, 'logout'])->name('logout');
 });
 
 
-Route::get('/terras', 'web\TerraController@index')->name('terras');
+Route::get('/terras', [TerraController::class, 'index'])->name('terras');
 Route::prefix('/terra')->group(function () {
-    Route::get('/criar', 'web\TerraController@redirecionaCriarTerra')->name('redireciona.criar.terra');
-    Route::post('/criar', 'web\TerraController@criarTerra')->name('criar.terra');
-    Route::get('/{idTerra}', 'web\TerraController@redirecionaListarTerra')->name('listar.terra');
-    Route::get('/editar/{idTerra}', 'web\TerraController@redirecionaEditarTerra')->name('redireciona.editar.terra');
-    Route::post('/editar', 'web\TerraController@editarTerra')->name('editar.terra');
-    Route::get('/remover/{idTerra}', 'web\TerraController@removerTerra')->name('remover.terra');
+    Route::get('/criar', [TerraController::class, 'redirecionaCriarTerra'])->name('redireciona.criar.terra');
+    Route::post('/criar', [TerraController::class, 'criarTerra'])->name('criar.terra');
+    Route::get('/{idTerra}', [TerraController::class, 'redirecionaListarTerra'])->name('listar.terra');
+    Route::get('/editar/{idTerra}', [TerraController::class, 'redirecionaEditarTerra'])->name('redireciona.editar.terra');
+    Route::post('/editar', [TerraController::class, 'editarTerra'])->name('editar.terra');
+    Route::get('/remover/{idTerra}', [TerraController::class, 'removerTerra'])->name('remover.terra');
 
-    Route::post('/remover/imagem', 'web\TerraController@removerImagem')->name('remover.imagem');
+    Route::post('/remover/imagem', [TerraController::class, 'removerImagem'])->name('remover.imagem');
 });
 
-Route::get('/quizzes', 'web\QuizController@index')->name('quizzes');
+Route::get('/quizzes', [QuizController::class, 'index'])->name('quizzes');
 Route::prefix('/quiz')->group(function () {
-    Route::get('/criar', 'web\QuizController@redirecionaCriarQuiz')->name('redireciona.criar.quiz');
-    Route::post('/criar', 'web\QuizController@criarQuiz')->name('criar.quiz');
-    Route::get('/editar/{idQuiz}', 'web\QuizController@redirecionaEditarQuiz')->name('redireciona.editar.quiz');
-    Route::post('/editar', 'web\QuizController@editarQuiz')->name('editar.quiz');
-    Route::post('/remover', 'web\QuizController@removerQuiz')->name('remover.quiz');
+    Route::get('/criar', [QuizController::class, 'redirecionaCriarQuiz'])->name('redireciona.criar.quiz');
+    Route::post('/criar', [QuizController::class, 'criarQuiz'])->name('criar.quiz');
+    Route::get('/editar/{idQuiz}', [QuizController::class, 'redirecionaEditarQuiz'])->name('redireciona.editar.quiz');
+    Route::post('/editar', [QuizController::class, 'editarQuiz'])->name('editar.quiz');
+    Route::post('/remover', [QuizController::class, 'removerQuiz'])->name('remover.quiz');
 });
 
-Route::get('/administradores', 'web\AdministradorController@index')->name('administradores');
+Route::get('/administradores', [AdministradorController::class, 'index'])->name('administradores');
 Route::prefix('/administrador')->group(function () {
-    Route::get('/criar', 'web\AdministradorController@redirecionaCriarAdministrador')->name('redireciona.criar.administrador');
-    Route::post('/criar', 'web\AdministradorController@criarAdministrador')->name('criar.administrador');
-    Route::get('/editar/{idAdmin}', 'web\AdministradorController@redirecionaEditarAdministrador')->name('redireciona.editar.administrador');
-    Route::post('/editar', 'web\AdministradorController@editarAdministrador')->name('editar.administrador');
-    Route::post('/remover', 'web\AdministradorController@removerAdministrador')->name('remover.administrador');
+    Route::get('/criar', [AdministradorController::class, 'redirecionaCriarAdministrador'])->name('redireciona.criar.administrador');
+    Route::post('/criar', [AdministradorController::class, 'criarAdministrador'])->name('criar.administrador');
+    Route::get('/editar/{idAdmin}', [AdministradorController::class, 'redirecionaEditarAdministrador'])->name('redireciona.editar.administrador');
+    Route::post('/editar', [AdministradorController::class, 'editarAdministrador'])->name('editar.administrador');
+    Route::post('/remover', [AdministradorController::class, 'removerAdministrador'])->name('remover.administrador');
 });
 
 
