351c9f4fc143ce8a2d3ca9180904eca6def8b842
Hiury Lima <limahiury6@gmail.com>
2023-09-27 19:53:25 -0300

    Atualizando textos da tela de gerência de Quiz..

--- web/resources/views/dashboard/quizzes/listar-quizzes.blade.php
+++ web/resources/views/dashboard/quizzes/listar-quizzes.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main')
-@section('title', 'Todos quizzes - Abaeté')
+@section('title', 'Todas perguntas - Abaeté')
 @section('content')
 <div id="root">
     <div id="page-map">
         <aside>
             <header>
                 <img src="{{ asset('images/map-marker.svg') }}" alt="Abaeté">
-                <h2>Cadastre um quiz</h2>
+                <h2>Cadastre uma pergunta</h2>
                 <p>Após, os cadastrados irão aparecer na tabela ao lado.</p>
             </header>
             <footer>
@@ -63,14 +63,14 @@
                     </div>
                     <div class="col col-1" data-label="Pontos">{{$quiz->pontos}}</div>
                     <div class="col col-1" data-label="Ação" style="display: flex; align-items: center;">
-                        <button id="editar-quiz" title="Editar quiz">
+                        <button id="editar-quiz" title="Editar pergunta">
                             <a href="{{ route('redireciona.editar.quiz', ['idQuiz' => $quiz->idQuiz]) }}">
                                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" color="#FFF" xmlns="http://www.w3.org/2000/svg" style="color: rgb(255, 255, 255);">
                                     <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path>
                                 </svg>
                             </a>
                         </button>
-                        <button id="remover-quiz" class="remover-quiz" title="Remover quiz" data-quiz="{{$quiz->idQuiz}}">
+                        <button id="remover-quiz" class="remover-quiz" title="Remover pergunta" data-quiz="{{$quiz->idQuiz}}">
                             <svg id="Icons" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <defs>
                                     <style>.cls-1{fill:#FFFFFF;}</style>
@@ -86,7 +86,7 @@
         </div>
 
 
-        <a class="criar-admin" href="{{ route('redireciona.criar.quiz') }}" title="Criar quiz">
+        <a class="criar-admin" href="{{ route('redireciona.criar.quiz') }}" title="Criar pergunta">
             <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" color="#FFF" height="32" width="32" xmlns="http://www.w3.org/2000/svg" style="color: rgb(255, 255, 255);">
                 <line x1="12" y1="5" x2="12" y2="19"></line>
                 <line x1="5" y1="12" x2="19" y2="12"></line>
@@ -100,7 +100,7 @@
                 <button id="fecharBtn"> X </button>
             </div>
             <div class="title">
-                <h1>Você tem certeza que você deseja remover esse Quiz?</h1>
+                <h1>Você tem certeza que você deseja remover essa pergunta?</h1>
             </div>
             <div class="body">
                 <p><strong>Atenção!</strong> Não será possível recuperar os registros depois.</p>
