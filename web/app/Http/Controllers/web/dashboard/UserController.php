a58d8309f81e9fdb014d574b09c21df2fb5467cc
Hiury Lima <limahiury6@gmail.com>
2023-09-27 10:10:40 -0300

    Atualizar busca do usuário autenticado.

--- web/app/Http/Controllers/web/dashboard/UserController.php
+++ web/app/Http/Controllers/web/dashboard/UserController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\web\dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $user = User::find(session()->get('id'));
+        $user = User::find(Auth::id());
         $administradores = User::where('admin', '=', true)->get();
 
         if (!$administradores->count()) {
