97171f4a7007327aa0b6d54bef16ec0f254d5d3d
Hiury Lima <limahiury6@gmail.com>
2023-10-04 19:20:55 -0300

    Adicionando o 'history.back()' nos botões de voltar.

--- web/resources/views/dashboard/quizzes/editar-quiz.blade.php
+++ web/resources/views/dashboard/quizzes/editar-quiz.blade.php
@@ -7,7 +7,7 @@
             <img src="{{ asset('images/map-marker.svg') }}" alt="Abaeté">
 
             <footer>
-                <a href="{{ route('quizzes') }}" title="Voltar">
+                <a onclick="history.back()" title="Voltar">
                     <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" color="rgba(0, 0, 0, 0.6)" height="24" width="24" xmlns="http://www.w3.org/2000/svg" style="color: rgba(0, 0, 0, 0.6);">
                         <line x1="19" y1="12" x2="5" y2="12"></line>
                         <polyline points="12 19 5 12 12 5"></polyline>
